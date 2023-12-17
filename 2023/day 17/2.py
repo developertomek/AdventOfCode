a1fae6917ebfbcf1100b66ca3709ec6382d7e574
developertomek <tomekcodes@gmail.com>
2023-12-17 15:58:46 +0100

    day 17

---
+++ 2023/day 17/2.py
@@ -0,0 +1,39 @@
+from heapq import heappush, heappop
+import os
+
+file_path = os.path.join('./2023/day 17', 'input.txt')
+file = open(file_path, 'r').read().splitlines()
+
+grid = [list(map(int, line.strip())) for line in file]
+
+seen = set()
+pq = [(0, 0, 0, 0, 0, 0)]
+
+while pq:
+    hl, r, c, dr, dc, n = heappop(pq)
+
+    if r == len(grid) - 1 and c == len(grid[0]) - 1 and n >= 4:
+        print(hl)
+        break
+
+    if (r, c, dr, dc, n) in seen:
+        continue
+
+    seen.add((r, c, dr, dc, n))
+
+    if n < 10 and (dr, dc) != (0, 0):
+        nr = r + dr
+        nc = c + dc
+
+        if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+            heappush(pq, (hl + grid[nr][nc], nr, nc, dr, dc, n + 1))
+
+
+    if n >= 4 or (dr, dc) == (0, 0):
+        for ndr, ndc in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+            if (ndr, ndc) != (dr, dc) and (ndr, ndc) != (-dr, -dc):
+                nr = r + ndr
+                nc = c + ndc
+
+                if 0 <= nr < len(grid) and 0 <= nc < len(grid[0]):
+                    heappush(pq, (hl + grid[nr][nc], nr, nc, ndr, ndc, 1))
